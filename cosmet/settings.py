5d4ab30447562bb4a7974a6d0e0f5a2cdeb227de
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2024-08-14 16:02:26 +0200

    Update

--- cosmet/settings.py
+++ cosmet/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cosmetapp',
-    'templates'
+    'templates',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
