d11de83588ee13c41f87eb7728281d692d7dafd5
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2024-11-02 18:17:59 +0100

    Update unit tests

--- cosmetapp/_test.py
+++ cosmetapp/_test.py
@@ -1,9 +1,8 @@
 import pytest
 from django.urls import reverse
 from django.contrib.auth.models import User
-from .models import NumberPhone
+from .models import Category, Order, NumberPhone
 
-# Fixture
 @pytest.fixture
 def client(db):
     from django.test import Client
@@ -18,7 +17,6 @@ def logged_in_client(client, user):
     client.login(username='testuser', password='testpassword')
     return client
 
-# Testy dla rejestracji i logowania (SignUpLoginTests)
 def test_signup_view(client):
     response = client.get(reverse('signup'))
     assert response.status_code == 200
@@ -37,9 +35,8 @@ def test_login_view(client):
 
 def test_logout_view(logged_in_client):
     response = logged_in_client.get(reverse('logout'))
-    assert response.status_code == 302  # Powinno przekierować na stronę logowania
+    assert response.status_code == 302  
 
-# Testy dla zamówień (OrderTests)
 def test_order_add_order_view(logged_in_client):
     response = logged_in_client.get(reverse('add_order'))
     assert response.status_code == 200
@@ -48,7 +45,6 @@ def test_order_orders_by_request_user(logged_in_client):
     response = logged_in_client.get(reverse('orders'))
     assert response.status_code == 200
 
-# Testy dla numerów telefonów (NumberPhoneTests)
 def test_number_phone_add_number_phone_view(logged_in_client):
     response = logged_in_client.get(reverse('add_number_phone'))
     assert response.status_code == 200
@@ -61,3 +57,21 @@ def test_number_phone_update_number_phone_view(logged_in_client, user):
 def test_number_phone_by_request_user(logged_in_client):
     response = logged_in_client.get(reverse('number_phone'))
     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_category_creation():
+    category = Category.objects.create(name='Test Category')
+    assert category.name == 'Test Category'
+
+@pytest.mark.django_db
+def test_order_creation(user):
+    category = Category.objects.create(name='Test Category')
+    order = Order.objects.create(user=user, category=category)
+    assert order.user == user
+    assert order.category == category
+
+@pytest.mark.django_db
+def test_number_phone_creation(user):
+    number_phone = NumberPhone.objects.create(user=user, number_phone='123456789')
+    assert number_phone.user == user
+    assert number_phone.number_phone == '123456789'
